0ed92fdaf638a98d01401a7c85b2c35f1bbcdb31
Tarun-coder <52537301+Tarun-coder@users.noreply.github.com>
2019-11-06 20:29:17 +0530

    modified with op

--- To check for Prime number.py
+++ To check for Prime number.py
@@ -3,6 +3,11 @@
 
 number=input("Enter the Number between 1 to 10: ")
 number=int(number)
+if number>0 and number<10:
+    pass
+else:
+    print("Please Insert a Valid number between 1 and 10")
+    exit()
 
 print("---------------------To check for Prime Number-------------------------------")
 prime_number=[2,3,5,7,9]
@@ -11,4 +16,19 @@ if number in prime_number:
     print("The number",number,"is Prime number")
 else:
     print("oops!! Its not a Prime Number")
+    
+# wrong input result
+Enter the Number between 1 and 10: 15
+Please Insert a Valid number between 1 and 10
+
+# output with specified limit
+Enter the Number between 1 and 10: 5
+--------------------To check for Prime Number-------------------------------
+The number 5 is Prime number
+
+# output when number is not Prime
+Enter the Number between 1 and 10: 4
+---------------------To check for Prime Number-------------------------------
+oops!! Its not a Prime Number
+
 
