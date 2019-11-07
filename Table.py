7d558b52028dc94a9de1b64ecbe23fad2a8d3add
Tarun-coder <Shomtarun007@gmail.com>
2019-11-07 13:56:56 +0530

    table

---
+++ Table.py
@@ -0,0 +1,9 @@
+# This help to determine the table of any Number
+number=int(input("Enter the number:"))
+print("The Table is")
+for i in range(1,11):
+    table=number*i
+    print(number,"x",i,"=",table)
+
+
+
