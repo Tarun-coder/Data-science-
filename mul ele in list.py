50bbf19c1e46240d7beefbb8969d7b6b42d0d8f2
Tarun-coder <Shomtarun007@gmail.com>
2019-11-08 12:43:22 +0530

    multiplication of element in list

---
+++ mul ele in list.py
@@ -0,0 +1,10 @@
+# Write a Python function to multiply all the numbers in a list.
+li=[1,2,3,4,5]
+
+def listsum():
+    mul=1
+    for i in list(li):
+        mul=mul*i
+    print("The multiplication of the list is : ",mul)
+     
+listsum()
