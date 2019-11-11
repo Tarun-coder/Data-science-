8f559a0bb4f951042029867e64848149a889d050
Tarun-coder <Shomtarun007@gmail.com>
2019-11-11 10:24:12 +0530

    modified range

--- nmber in list.py
+++ nmber in list.py
@@ -1,10 +1,10 @@
 # Write a Python function to check whether a number is in a given range.
 number=int(input("Enter the Number: "))
-def range(number):
-    li = [0,1,2,3,4,5,6,7,8,9]
+def checkinrange(number):
+    li = range(0,10000)
     if number in list(li):
-        print("Yes..The Number is in the Range of (0 to 9)")
+        print("Yes..The Number is in the Range of (0 to 10000)")
     else:
         print("oops!!...The Number is out Range")
 
-range(number)        
+checkinrange(number)        
