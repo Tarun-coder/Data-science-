7d7c6c0efd0f2850219584c910fb83329b4b6247
Tarun-coder <Shomtarun007@gmail.com>
2019-11-12 12:45:23 +0530

    Serialization

--- Serialization.py
+++ Serialization.py
@@ -1,13 +1,2 @@
 # Serialization : It is a process of converting structured data sets into object so that we can store that
 # into file and restored back whenever required
-import pickle
-print("---------------------------creating the list------------------------")
-
-li=[2,3,4,5,6,7,8]
-
-with open("pickle.txt","wb") as f:
-pickle.dump(li,f)
-
-del li
-
-print(li)
