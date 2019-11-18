4d58871f8fc9a13be7252cb8df25f31428ff1375
Tarun-coder <Shomtarun007@gmail.com>
2019-11-18 12:57:41 +0530

    list multiplication

--- numpy.py
+++ numpy.py
@@ -1,9 +1,11 @@
 print("----------------------------list sum---------------------------------")
-a=[35,20,3,4,5,6,7,8,5,30]
-c=a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]
-print("The Sum is :",c)
+a=[35,20,3,4,5,6,85,8,5,30]
+add=a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]
+mul=a[0]*a[1]*a[2]*a[3]*a[4]*a[5]*a[6]*a[7]*a[8]*a[9]
+print("The Sum is :",add)
+print("The Multiplication of The List Element is :",mul)
 
-print("----------------------Pattern-----------------------------------------")
+print("----------------------Random-Pattern ---------------------------------")
 
 for r in range(1,5,1):
     for s in range(r):
@@ -17,3 +19,7 @@ for r in range(1,5,1):
     for c in range(4):
         print("*",end=" ")
     print()  
+
+
+
+    
