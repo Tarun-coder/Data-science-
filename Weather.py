d0b385a9ea2f42ce3b82bbd46b601d35adb5a532
Tarun-coder <Shomtarun007@gmail.com>
2019-11-23 20:31:47 +0530

    Weather on Given Day

---
+++ Weather.py
@@ -0,0 +1,3 @@
+weather={"sunday":30,"Monday":22,"Tuesday":20,"Wednesday":15,"Thrusday":22,"Friday":18,"Saturday":24,"Sunday":16}
+day=input("Enter the Day to Find Temperature: ")
+print("The Temperature on ",day,"is ",weather[day],"degree")
