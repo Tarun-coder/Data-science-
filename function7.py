1a385cbe8c04f886d129915ce4c85b02a210634d
Tarun-coder <Shomtarun007@gmail.com>
2019-11-29 20:45:02 +0530

    Area and Permiter of Rectangle

---
+++ function7.py
@@ -0,0 +1,6 @@
+# Write a function to calculate area and perimeter of a rectangle.
+lenth=int(input("Enter the Lenth of Rectangle:"))
+width=int(input("Enter the Breadth of the Rectangle:"))
+
+print("The Permiter of the Rectangle is:",2*lenth+2*width,"cm")
+print("The Area of the Rectangle is:",lenth*width,"cm*2")
