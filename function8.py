7d55118de14d680de1a87c59ec4098f60569b4f1
Tarun-coder <Shomtarun007@gmail.com>
2019-11-29 21:00:46 +0530

    power of number

--- function8.py
+++ function8.py
@@ -1,1 +1,7 @@
 # Write a function to calculate power of a number raised to other. E.g.- ab.
+num=int(input("Enter the Number:"))
+power=int(input("Enter the Power:"))
+def exponent(num,power):
+    print("The value will be:",num**power**2)
+
+exponent(num,power)    
